902916892207767314c45fd0f500a9bee80fe2b7
sperret <sebastienperret93160@gmail.com>
2019-03-04 18:54:11 +0100

    add phpunit to project and remove deprecadted notices
    remove deprecated configuration in config

--- src/Anaxago/CoreBundle/Controller/Api/ProjectController.php
+++ src/Anaxago/CoreBundle/Controller/Api/ProjectController.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
-use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\View;
 
@@ -17,7 +17,7 @@ use FOS\RestBundle\Controller\Annotations\View;
  *
  * @package Anaxago\CoreBundle\Controller
  */
-class ProjectController extends FOSRestController
+class ProjectController extends AbstractFOSRestController
 {
     /**
      * @return \FOS\RestBundle\View\View
